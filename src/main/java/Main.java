0442d198f3f83d09708fdfad221fe03a5998dc9a
Ivan Shtroh <57430358+Ivan-Shtroh@users.noreply.github.com>
2021-03-24 17:02:58 +0700

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Operations op1 = new Operations();
-        op1.operation("input-search.json", "output-search.json", "search");
-        Operations op2 = new Operations();
-        op2.operation("input-stat.json", "output-stat.json", "stat");
+        String type = getType();
+        String input = getInput();
+        String output = getOutput();
+        Operations op = new Operations();
+        op.operation(input, output, type);
+    }
+
+    public static String getType() {
+        System.out.println("Operation type: ");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+    public static String getInput() {
+        System.out.println("Link to the input file");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+    public static String getOutput() {
+        System.out.println("Link to the output file");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
     }
 }
